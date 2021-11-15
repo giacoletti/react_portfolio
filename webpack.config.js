9ce8cf659ffc61d5ee7cb12133339fee8c2c7026
giacoletti <giacoletti92@gmail.com>
2021-11-15 16:09:55 +0100

    Removing --inline from start script in package.json, getting errors in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         loader: "babel-loader",
-        option: { presets: ["@babel/env"] }
+        options: { presets: ["@babel/env"] }
       },
       {
         test: /\.css$/,
